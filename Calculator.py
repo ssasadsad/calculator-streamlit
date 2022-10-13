9d31681168f58905122965f96609be58ddb8eeb7
ssasadsad <101778703+ssasadsad@users.noreply.github.com>
2022-10-13 20:11:09 +0700

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from PIL import Image
+
+logo = Image.open("C:\streamlit\photo\logo2.png")
+
+st.set_page_config(
+    page_icon=logo
+)
+
+
+def kali_kalian_def(): 
+    if kali_kalian == "+":
+        st.success(f"Hasil {input_num1 + input_num2}")
+    elif kali_kalian == "x":
+        st.success(f"Hasil {input_num1 * input_num2}")
+    elif kali_kalian == "-":
+        st.success(f"Hasil {input_num1 - input_num2}")
+    elif kali_kalian == "/":
+        st.success(f"Hasil {input_num1 / input_num2}")
+
+st.title(body="Calculator")
+input_num1=  st.number_input(label="Masukan angka1: ", value=0)
+kali_kalian= st.selectbox(
+    "Pilih",
+    ("+", "x", "-", "/")
+)
+input_num2=  st.number_input(label="Masukan angka2: ", value=0)
+st.header(body="\n")
+button = st.button(label="Submit")
+if button:
+    kali_kalian_def()
